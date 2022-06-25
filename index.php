03dd4a5da01430867c8e3121ffc33ff453ed7fd1
Demóstenes Filho <58401504+demostenesfilho@users.noreply.github.com>
2022-06-25 16:19:52 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ClickCenter</title>
+</head>
+<body>
+    <h1>Sistema ainda em desenvolvimento</h1>
+    <p>Deseja continuar?</p>
+    <a href="loja.html">Clique aqui para continuar</a>
+</body>
+</html>
